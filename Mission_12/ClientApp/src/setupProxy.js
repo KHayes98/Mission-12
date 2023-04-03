8036d97379bc42985be96d97227b4429ac79d0a2
Reboot <kchayes2016@gmail.com>
2023-04-03 11:18:25 -0600

    Push

--- Mission_12/ClientApp/src/setupProxy.js
+++ Mission_12/ClientApp/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const { env } = require('process');
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:14164';
+  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:50852';
 
 const context =  [
   "/weatherforecast",
